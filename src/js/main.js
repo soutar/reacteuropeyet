0dc636e367124647460ab8e6028f0444afb03644
BuildTools <john@soutar.me>
2015-05-10 15:19:23 +0100

    Updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
 (() => {
-  var element = document.getElementById('hello-world');
-  element.innerHTML = 'Hello world!';
+  var jsonPre = document.querySelector('.json-file');
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/package.json');
+  xhr.send(null);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      jsonPre.innerHTML = xhr.responseText;
+    }
+  }
 })();
